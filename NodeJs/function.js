fb9ce34986f219c84b2d1b2ce280efb2bc31c839
Orhan Bozgeyik <orhanbzgyk17@gmail.com>
2024-07-27 20:29:51 +0300

    NodeJs Start

---
+++ NodeJs/function.js
@@ -0,0 +1,15 @@
+function multiply(num1, num2) {
+
+    // It returns the multiplication
+    // of num1 and num2
+    return num1 * num2;
+}
+
+// Declare variable
+let x = 210000000;
+let y = 300000000;
+
+// Display the answer returned by
+// multiply function
+console.log("Multiplication of", x,
+    "and", y, "is", multiply(x, y));
