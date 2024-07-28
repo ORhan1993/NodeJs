af75d938296f2c4389ad537141e8735c4e272d7b
ORhan1993 <orhanbzgyk17@gmail.com>
2024-07-28 22:09:51 +0300

    Yaparız :D

--- NodeJs/First Application/firstprogram.js
+++ NodeJs/First Application/firstprogram.js
@@ -7,7 +7,7 @@ http.createServer(function(req,res){
 
     // HTTP Status: 200 : OK
     // Content Type: text/html
-    res.writeHead(200,{'Content Type': 'text/html'});
+    res.writeHead(200,{'Content-Type': 'text/html'});
 
     // Send the response body as "Hello World!"
     res.end('Hello World!');
