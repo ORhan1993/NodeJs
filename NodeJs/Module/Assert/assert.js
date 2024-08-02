2864ed6b4ec104851dd781ea588bd7526d24aa9c
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-02 11:41:09 +0300

    assert Tamam :)

---
+++ NodeJs/Module/Assert/assert.js
@@ -0,0 +1,28 @@
+/*
+
+console.clear() 
+const assert = require('assert'); 
+
+let x = 4; 
+let y = 5; 
+
+try { 
+
+	// Checking condition 
+	assert(x == y); 
+} 
+catch { 
+
+	// Error output 
+	console.log( 
+		`${x} is not equal to ${y}`); 
+} 
+
+*/
+console.clear() 
+const assert = require('assert'); 
+  
+let z = 4; 
+let k = 5; 
+  
+assert(z > k); 
