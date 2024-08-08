f874abc215ccb2aef1e8b8ed541f378e6cbb8670
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-08 14:37:27 +0300

    Yer Ayırma :)

---
+++ NodeJs/Module/Buffer/alloc.js
@@ -0,0 +1,9 @@
+// Node.js program to demonstrate the 
+// Buffer.alloc() Method 
+
+// Allocate buffer of given size 
+// using buffer.alloc() method 
+var buf = Buffer.alloc(6, 'a'); 
+
+// Prints <Buffer 61 61 61 61 61> 
+console.log(buf); 
