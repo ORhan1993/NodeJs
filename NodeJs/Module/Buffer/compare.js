5346e47001e0a33d1cf466964f8f656e13a1c4a5
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-08 11:54:24 +0300

    Karşılaştır :D

--- NodeJs/Module/Buffer/compare.js
+++ NodeJs/Module/Buffer/compare.js
@@ -2,12 +2,12 @@
 // Buffer.compare() Method
 
 // Creating a buffer
-const buffer1 = Buffer.from('Geek');
-const buffer2 = Buffer.from('Geek');
+const buffer1 = Buffer.from('Soft');
+const buffer2 = Buffer.from('Soft');
 const op = Buffer.compare(buffer1, buffer2);
 console.log('Doğru ise : '+op);
 
-const buffer3 = Buffer.from('GFG');
-const buffer4 = Buffer.from('Python');
+const buffer3 = Buffer.from('Soft');
+const buffer4 = Buffer.from('Hard');
 const op1 = Buffer.compare(buffer3, buffer4);
 console.log('Farklı ise : '+op1);
