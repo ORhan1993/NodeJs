f7df59f86b26f8858f0b9d6f381b5704f2ef7645
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-08 15:07:06 +0300

    Karşılaştırma :)

---
+++ NodeJs/Module/Buffer/equals.js
@@ -0,0 +1,9 @@
+// Node.js program to demonstrate the 
+// Buffer.equals() Method 
+
+// Create two bufferes 
+var buf1 = Buffer.from('Hi'); 
+var buf2 = Buffer.from('Hello'); 
+
+// Prints false(boolean value) 
+console.log(buf1.equals(buf2)); 
