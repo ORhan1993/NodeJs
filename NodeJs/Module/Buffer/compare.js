e2cd2a002b153d6ec544c4b562a0873e52851b2b
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-08 11:58:42 +0300

    Karşılaştırmaya devam :)

--- NodeJs/Module/Buffer/compare.js
+++ NodeJs/Module/Buffer/compare.js
@@ -1,4 +1,4 @@
-// Node.js program to demonstrate the
+/*// Node.js program to demonstrate the
 // Buffer.compare() Method
 
 // Creating a buffer
@@ -11,3 +11,19 @@ const buffer3 = Buffer.from('Soft');
 const buffer4 = Buffer.from('Hard');
 const op1 = Buffer.compare(buffer3, buffer4);
 console.log('Farklı ise : '+op1);
+*/
+
+// Node.js program to demonstrate the
+// Buffer.compare() Method
+
+// Creating a buffer
+const buffer1 = Buffer.from('2');
+const buffer2 = Buffer.from('1');
+const buffer3 = Buffer.from('3');
+const array = [buffer1, buffer2, buffer3];
+
+// Before sorting
+console.log('Sıralamdan Önce '+array);
+
+// After sorting array
+console.log('Sıralamadan Sonra '+array.sort(Buffer.compare));
