a59d939043f925611465955cfba5982d344e5970
ORhan1993 <orhanbzgyk17@gmail.com>
2024-08-09 11:47:21 +0300

    düzenleme :)

--- NodeJs/Module/Buffer/allocUnsafe.js
+++ NodeJs/Module/Buffer/allocUnsafe.js
@@ -1,21 +1,11 @@
-// Node.js program to demonstrate the 
-// Buffer.allocUnsafeSlow() Method 
-	
-// Creating a buffer 
-const buffer = Buffer.allocUnsafe(4); 
-	
-// Print: random string everytime we run the 
-// program as we have not added 
-// anything to the buffer yet 
-console.log(buffer.toString()); 
 
-for (let i = 0; i < 4; i++) { 
+	// Node.js program to demonstrate the 
+	// Buffer.cd() Method 
+		
+	// Creating a buffer 
+	const buffer = Buffer.allocUnsafe(10); 
+		
+	// Display the buffer containing random values 
+	console.log("allocUnsafeSlow() Method"); 
+	console.log(buffer); 
 
-	//filling the values in buffer 
-	buffer[i] = i + 97; 
-    console.log(buffer[i]);
-} 
-
-// Adds and Print: 'abcd' as 97 98 99 100 101 
-// are their respective ASCII values 
-console.log(buffer.toString()); 
--- NodeJs/Module/Buffer/allocUnsafeSlow.js
+++ NodeJs/Module/Buffer/allocUnsafeSlow.js
@@ -1,11 +1,21 @@
+// Node.js program to demonstrate the 
+// Buffer.allocUnsafeSlow() Method 
+	
+// Creating a buffer 
+const buffer = Buffer.allocUnsafeSlow(4); 
+	
+// Print: random string everytime we run the 
+// program as we have not added 
+// anything to the buffer yet 
+console.log(buffer.toString()); 
 
-	// Node.js program to demonstrate the 
-	// Buffer.cd() Method 
-		
-	// Creating a buffer 
-	const buffer = Buffer.allocUnsafe(10); 
-		
-	// Display the buffer containing random values 
-	console.log("allocUnsafeSlow() Method"); 
-	console.log(buffer); 
+for (let i = 0; i < 4; i++) { 
 
+	//filling the values in buffer 
+	buffer[i] = i + 97; 
+    console.log(buffer[i]);
+} 
+
+// Adds and Print: 'abcd' as 97 98 99 100 101 
+// are their respective ASCII values 
+console.log(buffer.toString()); 
